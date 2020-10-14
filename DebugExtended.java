5acfeaca26b7c17840a48b341e0c21e93994aa5e
tcoleman5 <47992464+tcoleman5@users.noreply.github.com>
2020-10-14 15:27:31 -0500

    Update DebugExtended.java

--- DebugExtended.java
+++ DebugExtended.java
@@ -3,13 +3,16 @@ package com.company;
 import java.util.Scanner;
 
 public class Debug {
+            
             public static void main(String[] args) {
 
                 Scanner input=new Scanner(System.in);
                 System.out.print("Input your name: ");
                 String name=input.nextLine();
-                System.out.print("How was your day? ");
-                String day=input.nextLine();
-                System.out.println("Glad to hear that your day is going " +day+ "," +name;
+                System.out.print("Enter your age: ");
+                String Age=input.nextLine();
+                System.out.print("Enter your DOB year: ");
+                String dob = input.nextLine();
+                System.out.println("Summary: "+"Your name is: "+name+"\n"+"Your age is: "+Age+"\n"+"Your birth year is: "+dob);
     }
 }
