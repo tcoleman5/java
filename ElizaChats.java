f7c466f3a24c0f0ab6eb7ba9c0c8766fe5bc34ef
tcoleman5 <47992464+tcoleman5@users.noreply.github.com>
2020-10-14 15:36:39 -0500

    Create ElizaChats.java

---
+++ ElizaChats.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to ElizaChats! My name is Eliza, What's your name?");
+        String name = input.nextLine();
+        System.out.println("Nice to meet you, "+name+", How has your day been?");
+        String dayfeels = input.nextLine();
+        System.out.println("Okay, Anything in particular that makes you feel that "+dayfeels+" ?");
+        String explain = input.nextLine();
+        System.out.println("Okay, Well it has been my pleasure to speak with you. Have a nice day!");
+
+        System.out.println("Here are your responses: "+name+" "+dayfeels+" "+explain+" :)");
