5378312542a2377bb5bdea9bef594cbb9b4732a1
tcoleman5 <47992464+tcoleman5@users.noreply.github.com>
2020-10-21 14:33:13 -0500

    Create mario.java

---
+++ mario.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int blocks = 0;
+        do { System.out.print("How many bricks do you want ");
+            blocks = input.nextInt() 
+            
+            } while ((blocks < 1) || (blocks > 8));
+        {
+            for (int row = 0; row < blocks; row++) {
+                for (int space = 0; space < (blocks-row-1); space++){
+                    System.out.print(" ");
+                }
+                for (int col =0; col <= row; col++ ){
+                    System.out.print("#");
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+}
