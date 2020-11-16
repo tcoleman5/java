f028f8d5c123aa372179e143e8ac8716a5a589d8
tcoleman5 <47992464+tcoleman5@users.noreply.github.com>
2020-11-16 15:30:36 -0600

    Create cash.java

---
+++ cash.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+	  Scanner input = new Scanner(System.in);
+    float changemin = 0.0f;
+    
+	  do {
+		System.out.print("How much change is owed? ");
+		changemin = input.nextFloat();
+	    } while (changemin < 0.0);
+       
+	      int cents = Math.round(changemin * 100);
+        
+      if (cents / 25 >= 1) {  
+	      int coins = (changemin / 25);
+	      int rem = changemin % 25; }
+        
+      else if (cents /10 >= 1) {  
+        int coins = (changemin / 10);
+	      int rem = changemin % 10; }
+        
+      else if (cents / 5 >= 1) {
+	      int coins = (changemin / 5);
+	      int rem = changemin % 5; }
+      
+      else {
+	      int coins = (changemin / 1);
+        int rem = changemin % 1; }
+      
+	  System.out.println("The minimum number of coins is: " + coins);
+
+    }
+}
